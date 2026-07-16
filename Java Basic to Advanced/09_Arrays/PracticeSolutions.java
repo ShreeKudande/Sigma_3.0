801873f994782b95649ef13fa222eb33dc599744
Shree Kudande <shree.s.kudande@gmail.com>
2026-07-16 07:48:31 +0530

    Solved Basic Sorting Algorithms Pratice Questions

--- Java Basic to Advanced/09_Arrays/PracticeSolutions.java
+++ Java Basic to Advanced/09_Arrays/PracticeSolutions.java
@@ -2,9 +2,57 @@ import java.util.Arrays;
 
 public class PracticeSolutions {
 
-    
+    public static boolean containsDuplicate(int nums[]) {
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length-1; i++) {
+            if(nums[i] == nums[i+1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int searchInRotatedSortedArray(int nums[], int tar) {
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+
+            if(nums[mid] == tar) {
+                return mid;
+            }
+
+            if(nums[start] <= nums[mid]) { //left sorted
+                if(nums[start] <= tar && tar <= nums[mid]) {
+                    end = mid - 1;
+                } else{
+                    start = mid + 1;
+                }
+            } else { //right sorted
+                if(nums[mid] <= tar && tar <= nums[end]) {
+                    end = mid - 1;
+                } else{
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
 
     public static void main(String args[]) {
+        //Q1
+        // int[] nums = {1, 2, 3, 1};
+        // System.out.println(containsDuplicate(nums));
+
+        //Q2
+        // int[] nums = {3, 4, 5, 6, 7, 0, 1, 2};
+        // int tar = 0;
+        // System.out.println(searchInRotatedSortedArray(nums, tar));
+
+        //Q3,Q4 & Q5 would solve afterwards 
         
+
     }
 }
--- Java Basic to Advanced/10_BasicSortingAlgorithms/PracticeSolutions.java
+++ Java Basic to Advanced/10_BasicSortingAlgorithms/PracticeSolutions.java
@@ -1,9 +1,69 @@
+// public class PracticeSolutions {
+
+//     public static void bubbleSort(int arr[]) {
+//         for(int i = 0; i < arr.length; i++) {
+//             for(int j = 0; j < arr.length-i-1; j++) {
+//                 if(arr[j] < arr[j+1]) {
+//                     int temp = arr[j];
+//                     arr[j] = arr[j+1];
+//                     arr[j+1] = temp;
+//                 }
+//             }
+//         }
+//     }
+
+//     public static void selectionSort(int arr[]) {
+//         for(int i = 0; i < arr.length-1; i++) {
+//             int min = i;
+//             for(int j = i+1; j < arr.length; j++) {
+//                 if(arr[min] < arr[j]) {
+//                     min = j;
+//                 }
+//             }
+//             //swap
+//             int temp = arr[min];
+//             arr[min] = arr[i];
+//             arr[i] = temp;
+//         }
+//     }
+
+//     public static void insertionSort(int arr[]) {
+
+//     }
+
+//     public static void printArr(int arr[]) {
+//         for(int num : arr) {
+//             System.out.print(num + " ");
+//         }
+//     }
+
+//     public static void main(String args[]) {
+//         //Use the following sorting algorithms to sort an array in DESCENDING order:
+//         int arr[] = {3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
+
+//         //Bubble Sort
+//         // bubbleSort(arr);
+//         // printArr(arr);
+
+//         //Selection Sort
+//         selectionSort(arr);
+//         printArr(arr);
+
+//         //Insertion Sort
+//         insertionSort(arr);
+//         printArr(arr);
+
+//         //Counting Sort
+//     }
+// }
+
 public class PracticeSolutions {
 
     public static void bubbleSort(int arr[]) {
-        for(int i = 0; i < arr.length; i++) {
-            for(int j = 0; j < arr.length-i-1; j++) {
+        for(int turn = 0; turn < arr.length; turn++) {
+            for(int j = 0; j < arr.length-turn-1; j++) {
                 if(arr[j] < arr[j+1]) {
+                    //swap
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
@@ -12,28 +72,61 @@ public class PracticeSolutions {
         }
     }
 
+    public static void printArr(int arr[]) {
+        for(int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
     public static void selectionSort(int arr[]) {
-        for(int i = 0; i < arr.length-1; i++) {
-            int min = i;
+
+        for(int i = 0; i < arr.length; i++) {
+            int largest = i;
             for(int j = i+1; j < arr.length; j++) {
-                if(arr[min] < arr[j]) {
-                    min = j;
+                if(arr[j] > arr[largest]) {
+                    largest = j;
                 }
             }
             //swap
-            int temp = arr[min];
-            arr[min] = arr[i];
+            int temp = arr[largest];
+            arr[largest] = arr[i];
             arr[i] = temp;
         }
     }
 
     public static void insertionSort(int arr[]) {
-
+        for(int i = 1; i < arr.length; i++) {
+            int curr = arr[i];
+            int j = i-1;
+            while(j >= 0 && arr[j] < curr) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = curr;
+        }
     }
 
-    public static void printArr(int arr[]) {
-        for(int num : arr) {
-            System.out.print(num + " ");
+    public static void countingSort(int arr[]) {
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        int freq[] = new int[max+1];
+
+        for(int i = 0; i < arr.length; i++) {
+            freq[arr[i]]+=1;
+        }
+
+        int j = 0;
+        for(int i = freq.length-1; i >= 0; i--) {
+            while(freq[i] > 0) {
+                arr[j] = i;
+                j++;
+                freq[i]--;
+            }
         }
     }
 
@@ -43,16 +136,15 @@ public class PracticeSolutions {
 
         //Bubble Sort
         // bubbleSort(arr);
-        // printArr(arr);
 
         //Selection Sort
-        selectionSort(arr);
-        printArr(arr);
+        // selectionSort(arr);
 
         //Insertion Sort
-        insertionSort(arr);
-        printArr(arr);
+        // insertionSort(arr);
 
         //Counting Sort
+        countingSort(arr);
+        printArr(arr);
     }
 }
