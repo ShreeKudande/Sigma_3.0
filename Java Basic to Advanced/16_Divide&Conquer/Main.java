3c463c10f39e7be4926f0a4a944dcb85548db3b5
Shree Kudande <shree.s.kudande@gmail.com>
2026-08-16 12:28:52 +0530

    Completed Devide & Conquer Module

--- Java Basic to Advanced/16_Divide&Conquer/Main.java
+++ Java Basic to Advanced/16_Divide&Conquer/Main.java
@@ -50,11 +50,84 @@ public class Main {
         }
     }
 
+    public static void quickSort(int arr[], int si, int ei) {
+        if(si >= ei) {
+            return;
+        }
+
+        //last element
+        int pidx = partition(arr, si, ei);
+        quickSort(arr, si, pidx-1); //left
+        quickSort(arr, pidx+1, ei); //right
+    }
+
+    public static int partition(int arr[], int si, int ei) {
+        int pivot = arr[ei];
+        int i = si - 1; //to make place for els smaller than pivot
+        for(int j = si; j < ei; j++) {
+            if(arr[j] <= pivot) {
+                i++;
+                //swap
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        i++;
+        //swap
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;
+    }
+
+    public static int search(int nums[], int target, int si, int ei) {
+        if(si > ei) {
+            return -1;
+        }
+
+        int mid = si + (ei - si) / 2;
+
+        if(nums[mid] == target) {
+            return mid;
+        }
+
+        if(nums[si] <= nums[mid]) {
+            if(nums[si] <= target && target <= nums[mid]) {
+                return search(nums, target, si, mid-1);
+            } else {
+                return search(nums, target, mid+1, ei);
+            }
+        } else {
+            if(nums[mid] <= target && target <= nums[ei]) {
+                return search(nums, target, mid+1, ei);
+            } else {
+                return search(nums, target, si, mid-1);
+            }
+        }
+    }
+
     public static void main(String args[]) {
-        int arr[] = {6, 3, 9, 5, 2, 8};
-        printArr(arr);
-        mergeSort(arr, 0, arr.length-1);
-        System.out.println();
-        printArr(arr);
+
+        //merge sort
+        // int arr[] = {6, 3, 9, 5, 2, 8};
+        // printArr(arr);
+        // mergeSort(arr, 0, arr.length-1);
+        // System.out.println();
+        // printArr(arr);
+
+        //quick sort
+        // int arr[] = {6, 3, 9, 5, 2, 8};
+        // printArr(arr);
+        // quickSort(arr, 0, arr.length-1);
+        // System.out.println();
+        // printArr(arr);
+        //Worst Case --> worst case occurs when pivot is always the smallest or the largest element.
+
+        //Search in Rotated Sorted Array
+        int nums[] = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        int tarIdx = search(nums, target, 0, nums.length-1);
+        System.out.println(tarIdx);
     }
 }
