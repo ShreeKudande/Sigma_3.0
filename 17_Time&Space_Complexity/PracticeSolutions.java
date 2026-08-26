440b18c0f3024ae27389e1ab9431ca7a861832f5
Shree Kudande <shree.s.kudande@gmail.com>
2026-08-26 09:03:34 +0530

    Solved Time & Space Complexity Questions

---
+++ 17_Time&Space_Complexity/PracticeSolutions.java
@@ -0,0 +1,9 @@
+//a) ans --> O(n log n)
+//b) ans --> O(logkn)
+//c) ans --> False
+//d) ans --> 
+// Time complexity - O(√n)
+//Space complexity - O(1)
+//e) ans --> 
+//Time complexity - O(n^2)
+//Space complexity - O(1)
